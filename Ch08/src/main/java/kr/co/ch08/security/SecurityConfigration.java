7cdf6bc9e00c5dd76ecb1421174d2f16b4402009
094haley <094haley@gmail.com>
2023-01-13 19:50:29 +0900

    2023/01/13 학원

--- Ch08/src/main/java/kr/co/ch08/security/SecurityConfigration.java
+++ Ch08/src/main/java/kr/co/ch08/security/SecurityConfigration.java
@@ -2,57 +2,54 @@ package kr.co.ch08.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	// WebSecurityConfigurerAdapter 상속 받아서 하는 씨큐리티 설정은 Deprecated 됨 -> filterChain 방식 권장
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		// 인가(접근권한) 설정:
+@Configuration
+public class SecurityConfigration {
+
+	@Autowired
+	private SecurityUserService service;
+
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		// 인가(접근권한) 설정
 		http.authorizeHttpRequests().antMatchers("/").permitAll();
-		http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");
+		http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN"); 
 		http.authorizeHttpRequests().antMatchers("/member/**").hasAnyRole("ADMIN", "MEMBER");
 		http.authorizeHttpRequests().antMatchers("/user2/loginSuccess").hasAnyRole("3", "4", "5");
-		
+
 		// 사이트 위변조 요청 방지
 		http.csrf().disable();
-		
-		// 로그인 페이지 설정
+
+		// 로그인 설정
 		http.formLogin()
 		.loginPage("/user2/login")
 		.defaultSuccessUrl("/user2/loginSuccess")
-		.failureUrl("/user2/login?success=100")
+		.failureUrl("/user2/login?success=100)")
 		.usernameParameter("uid")
 		.passwordParameter("pass");
-		
+
 		// 로그아웃 설정
 		http.logout()
 		.invalidateHttpSession(true)
 		.logoutRequestMatcher(new AntPathRequestMatcher("/user2/logout"))
 		.logoutSuccessUrl("/user2/login?success=200");
-		
+
+		// 사용자 인증 처리 컴포넌트 서비스 등록
+		http.userDetailsService(service);
+
+		return http.build();
 	}
-	
-	@Autowired
-	private SecurityUserService service;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		// 로그인 인증처리 서비스 등록
-		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
-		
-	}
-	
+
+	@Bean
+    public PasswordEncoder PasswordEncoder () {
+        //return new MessageDigestPasswordEncoder("SHA-256");
+		return new BCryptPasswordEncoder();
+    }
 }
