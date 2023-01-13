1b931f18436e5141e3659f5f2ba2738ab90459ad
094haley <094haley@gmail.com>
2023-01-13 19:25:34 +0900

    2023/01/13 학원

--- Ch08/src/main/java/kr/co/ch08/security/SecurityConfig.java
+++ Ch08/src/main/java/kr/co/ch08/security/SecurityConfig.java
@@ -1,14 +1,59 @@
 package kr.co.ch08.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+	private SecurityUserService service;
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		// 인가(접근권한) 설정:
+		http.authorizeHttpRequests().antMatchers("/").permitAll();
+		http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");
+		http.authorizeHttpRequests().antMatchers("/member/**").hasAnyRole("ADMIN", "MEMBER");
+		http.authorizeHttpRequests().antMatchers("/user2/loginSuccess").hasAnyRole("3", "4", "5");
+		
+		// 사이트 위변조 요청 방지
+		http.csrf().disable();
+		
+		// 로그인 페이지 설정
+		http.formLogin()
+		.loginPage("/user2/login")
+		.defaultSuccessUrl("/user2/loginSuccess")
+		.failureUrl("/user2/login?success=100")
+		.usernameParameter("uid")
+		.passwordParameter("pass");
+		
+		// 로그아웃 설정
+		http.logout()
+		.invalidateHttpSession(true)
+		.logoutRequestMatcher(new AntPathRequestMatcher("/user2/logout"))
+		.logoutSuccessUrl("/user2/login?success=200");
+		
+	
+		return http.build();
+	}
+
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+/*
+ * 기존 방식 -> 권장되지 않음
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
@@ -52,3 +97,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 }
+*/
